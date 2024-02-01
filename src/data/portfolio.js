74e0484f3c63cef5029a66a4eca0a920f8431674
Samath Lenaduwa Lokuge <samath.lenaduwalokuge@aalto.fi>
2024-02-01 13:51:13 +0300

    submit button Commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,15 +1,15 @@
 export default [
     {
-        title: 'Github Page',
-        imgUrl: '/assets/github_profile.jpg',
-        stack: ['Repos', 'Projects', 'Links'],
-        link: 'https://github.com/Samath97'
-    },
-    {
         title: 'LinkedIn Page',
         imgUrl: '/assets/LinkedIn_profile.jpg',
         stack: ['Experiences', 'Skills', 'Degrees'],
         link: 'https://www.linkedin.com/in/samath-lokuge/'
+    },
+    {
+        title: 'Github Page',
+        imgUrl: '/assets/github_profile.jpg',
+        stack: ['Repos', 'Projects', 'Links'],
+        link: 'https://github.com/Samath97'
     },
     {
         title: 'Nature Page',
