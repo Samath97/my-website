1c62da76a55a4c2c7eeb9fdfa269375aab364934
Samath Lenaduwa Lokuge <samath.lenaduwalokuge@aalto.fi>
2024-02-01 15:52:33 +0300

    updated images Commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,20 +2,20 @@ export default [
     {
         title: 'LinkedIn Page',
         imgUrl: '/assets/LinkedIn_profile.jpg',
-        stack: ['Experiences', 'Skills', 'Degrees'],
+        stack: ['Experiences', 'Skills', 'Jobs'],
         link: 'https://www.linkedin.com/in/samath-lokuge/'
     },
     {
         title: 'Github Page',
         imgUrl: '/assets/github_profile.jpg',
-        stack: ['Repos', 'Projects', 'Links'],
+        stack: ['Repos', 'Projects', 'Skills'],
         link: 'https://github.com/Samath97'
     },
     {
         title: 'Nature Page',
         imgUrl: '/assets/forest_profile.jpg',
-        stack: ['Finnish', 'Nature', 'Wix'],
-        link: 'https://samathlokuge.wixsite.com/smmy/oma-kokemus'
+        stack: ['Finnish', 'Nature', 'Youth'],
+        link: 'https://samathlokuge.wixsite.com/smmy'
     }
 
 ]
