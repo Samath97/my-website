94875e03445ff7b460abcba19524bd472bfd9292
Samath Lenaduwa Lokuge <samath.lenaduwalokuge@aalto.fi>
2024-02-02 11:18:52 +0300

    add more links Commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -12,10 +12,29 @@ export default [
         link: 'https://github.com/Samath97'
     },
     {
-        title: 'Nature Page',
+        title: 'Unreal Projects',
+        imgUrl: '/assets/unreal_drive.jpg',
+        stack: ['C++', 'Unreal Engine', 'Games'],
+        link: 'https://drive.google.com/drive/folders/1_N0ICQH6mr3kNO4EU64JymAv6JFG-xLW?usp=sharing'
+    },
+    {
+        title: 'Automation game Car Models',
+        imgUrl: '/assets/automation_models.jpg',
+        stack: ['Automation game', 'Car models', 'Designing'],
+        link: 'https://drive.google.com/drive/folders/1olSjNmUjtLD4zP1gsn3dMOLVIHVGCTP4?usp=sharing'
+    },
+    {
+        title: 'Racing Game Site',
+        imgUrl: '/assets/racing_site.jpg',
+        stack: ['Need4Speed', 'Burnout', 'Gaming'],
+        link: 'https://nfsburnout.weebly.com/index.html'
+    },
+    {
+        title: 'Nature Site',
         imgUrl: '/assets/forest_profile.jpg',
         stack: ['Finnish', 'Nature', 'Youth'],
         link: 'https://samathlokuge.wixsite.com/smmy'
     }
 
 ]
+
